78fc44d4ee2afde2e245f025142bb5ed3427b090
SuWh1 <apasdauren70>
2024-10-17 22:30:42 +0500

    web application added && errors solved

--- main.go
+++ main.go
@@ -2,138 +2,199 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 	"os/exec"
 	"runtime"
 	"strings"
-	"time"
+	"text/template"
 
 	"github.com/shirou/gopsutil/v3/cpu"
 	"github.com/shirou/gopsutil/v3/disk"
 	"github.com/shirou/gopsutil/v3/mem"
 )
 
-func getGPUInfoLinux() string {
+type SystemInfo struct {
+	OS           string `json:"os"`
+	Architecture string `json:"architecture"`
+	Hostname     string `json:"hostname"`
+	NumCPU       int    `json:"num_cpu"`
+	GoVersion    string `json:"go_version"`
+
+	Memory MemoryInfo `json:"memory"`
+	CPU    CPUInfo    `json:"cpu"`
+	Disk   DiskInfo   `json:"disk"`
+
+	GPU []string `json:"gpu"`
+}
+
+type MemoryInfo struct {
+	Total       uint64  `json:"total"`
+	Free        uint64  `json:"free"`
+	Used        uint64  `json:"used"`
+	UsedPercent float64 `json:"used_percent"`
+}
+
+type CPUInfo struct {
+	ModelName string  `json:"model_name"`
+	Cores     int32   `json:"cores"`
+	Usage     float64 `json:"usage"`
+}
+
+type DiskInfo struct {
+	Total       uint64  `json:"total"`
+	Free        uint64  `json:"free"`
+	Used        uint64  `json:"used"`
+	UsedPercent float64 `json:"used_percent"`
+}
+
+func getGPUInfo() []string {
+	switch runtime.GOOS {
+	case "linux":
+		return getGPUInfoLinux()
+	case "windows":
+		return getGPUInfoWindows()
+	case "darwin":
+		return getGPUInfoMacOS()
+	default:
+		return []string{"GPU information not available for this OS."}
+	}
+}
+
+func getGPUInfoLinux() []string {
 	cmd := exec.Command("lspci")
 	output, err := cmd.Output()
 	if err != nil {
-		return fmt.Sprintf("Error executing lspci: %v\n", err)
-	}
-
-	outputStr := string(output)
-	lines := strings.Split(outputStr, "\n")
-
-	for _, line := range lines {
-		if strings.Contains(line, "VGA") {
-			return fmt.Sprintf("GPU Info: %s\n", line)
-		}
-	}
-	return "No GPU information found."
-}
-
-func getGPUInfoNvidia() string {
-	cmd := exec.Command("nvidia-smi", "--query-gpu=name,memory.total", "--format=csv")
-	output, err := cmd.Output()
-	if err != nil {
-		return fmt.Sprintf("Error executing nvidia-smi: %v\n", err)
-	}
-
-	return fmt.Sprintf("NVIDIA GPU Info:\n%s\n", string(output))
-}
-
-func getGPUInfoWindows() string {
-	cmd := exec.Command("powershell", "Get-WmiObject", "Win32_VideoController | Select-Object -Property Name,AdapterRAM | Format-Table -HideTableHeaders")
-	output, err := cmd.Output()
-	if err != nil {
-		return fmt.Sprintf("Error executing PowerShell command: %v", err)
-	}
-
-	result := strings.TrimSpace(string(output))
-	return fmt.Sprintf("Windows GPU Info:\n%s\n", result)
-}
-
-func getGPUInfoMacOS() string {
-	cmd := exec.Command("system_profiler", "SPDisplaysDataType")
-	output, err := cmd.Output()
-	if err != nil {
-		return fmt.Sprintf("Error executing system_profiler: %v", err)
+		return []string{fmt.Sprintf("Error executing lspci: %v", err)}
 	}
 
 	outputStr := string(output)
 	lines := strings.Split(outputStr, "\n")
 
 	var gpuInfo []string
+
+	for _, line := range lines {
+		if strings.Contains(line, "VGA") || strings.Contains(line, "3D") {
+			gpuInfo = append(gpuInfo, line)
+		}
+	}
+
+	if len(gpuInfo) == 0 {
+		return []string{"No GPU information found."}
+	}
+
+	return gpuInfo
+}
+
+func getGPUInfoWindows() []string {
+	cmd := exec.Command("powershell", "Get-WmiObject", "Win32_VideoController | Select-Object -Property Name,AdapterRAM | Format-Table -HideTableHeaders")
+	output, err := cmd.Output()
+	if err != nil {
+		return []string{fmt.Sprintf("Error executing PowerShell command: %v", err)}
+	}
+
+	result := strings.TrimSpace(string(output))
+	lines := strings.Split(result, "\n")
+
+	var gpuInfo []string
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		if line != "" { // Ensure that only non-empty lines are added
+			gpuInfo = append(gpuInfo, line)
+		}
+	}
+
+	if len(gpuInfo) == 0 {
+		return []string{"No GPU information found."}
+	}
+
+	return gpuInfo
+}
+
+func getGPUInfoMacOS() []string {
+	cmd := exec.Command("system_profiler", "SPDisplaysDataType")
+	output, err := cmd.Output()
+	if err != nil {
+		return []string{fmt.Sprintf("Error executing system_profiler: %v", err)}
+	}
+
+	outputStr := string(output)
+	lines := strings.Split(outputStr, "\n")
+
+	var gpuInfo []string
+
 	for _, line := range lines {
 		if strings.Contains(line, "Chipset Model") || strings.Contains(line, "VRAM") {
 			gpuInfo = append(gpuInfo, strings.TrimSpace(line))
 		}
 	}
-	return strings.Join(gpuInfo, "\n")
+
+	if len(gpuInfo) == 0 {
+		return []string{"No GPU information found."}
+	}
+
+	return gpuInfo
+}
+
+func systemInfoHandler(w http.ResponseWriter, r *http.Request) {
+	hostname, _ := os.Hostname()
+	virtualMemory, _ := mem.VirtualMemory()
+	cpuInfo, _ := cpu.Info()
+	cpuPercent, _ := cpu.Percent(0, false)
+	diskStat, _ := disk.Usage("/")
+
+	sysInfo := SystemInfo{
+		OS:           runtime.GOOS,
+		Architecture: runtime.GOARCH,
+		Hostname:     hostname,
+		NumCPU:       runtime.NumCPU(),
+		GoVersion:    runtime.Version(),
+		Memory: MemoryInfo{
+			Total:       virtualMemory.Total / 1024 / 1024,
+			Free:        virtualMemory.Free / 1024 / 1024,
+			Used:        virtualMemory.Used / 1024 / 1024,
+			UsedPercent: virtualMemory.UsedPercent,
+		},
+		CPU: CPUInfo{
+			ModelName: cpuInfo[0].ModelName,
+			Cores:     cpuInfo[0].Cores,
+			Usage:     cpuPercent[0],
+		},
+		Disk: DiskInfo{
+			Total:       diskStat.Total / 1024 / 1024 / 1024,
+			Free:        diskStat.Free / 1024 / 1024 / 1024,
+			Used:        diskStat.Used / 1024 / 1024 / 1024,
+			UsedPercent: diskStat.UsedPercent,
+		},
+		GPU: getGPUInfo(),
+	}
+
+	tmpl, err := template.ParseFiles("templates/system.html")
+	if err != nil {
+		fmt.Printf("Error parsing template: %v\n", err)
+		http.Error(w, "Error", http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(w, sysInfo); err != nil {
+		fmt.Printf("Error rendering template: %v\n", err)
+	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("templates/main.html")
+	if err != nil {
+		http.Error(w, "Error parsing template", http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, "Error rendering template", http.StatusInternalServerError)
+	}
 }
 
 func main() {
-	fmt.Println("==================")
-	fmt.Println("System Information")
-	fmt.Println("==================")
-	fmt.Printf("OS: %s\n", runtime.GOOS)
-	fmt.Printf("Architecture: %s\n", runtime.GOARCH)
-
-	host, err := os.Hostname()
-	if err != nil {
-		fmt.Printf("Error fetching hostname: %v\n", err)
-	} else {
-		fmt.Printf("Device name: %s\n", host)
-	}
-
-	fmt.Printf("Number of CPUs: %d\n", runtime.NumCPU())
-
-	fmt.Printf("Go Version: %s\n", runtime.Version())
-
-	virtualMem, _ := mem.VirtualMemory()
-	fmt.Println("\n==================")
-	fmt.Printf("Memory Information\n")
-	fmt.Println("==================")
-	fmt.Printf("Total: %v MB\n", virtualMem.Total/1024/1024)
-	fmt.Printf("Free: %v MB\n", virtualMem.Free/1024/1024)
-	fmt.Printf("Used: %v MB\n", virtualMem.Used/1024/1024)
-	fmt.Printf("Used Percent: %0.3f%%\n", virtualMem.UsedPercent)
-
-	fmt.Println("\n==================")
-	fmt.Printf("CPU Information\n")
-	fmt.Println("==================")
-	cpuInfo, _ := cpu.Info()
-	for _, info := range cpuInfo {
-		fmt.Printf("Model: %s\n", info.ModelName)
-		fmt.Printf("Cores: %d\n", info.Cores)
-	}
-	cpuPercent, _ := cpu.Percent(time.Second, false)
-	fmt.Printf("CPU Usage: %.2f%%\n\n", cpuPercent[0])
-
-	fmt.Println("==================")
-	fmt.Printf("Disk Information\n")
-	fmt.Println("==================")
-	diskStat, _ := disk.Usage("/")
-	fmt.Printf("Total: %v GB\n", diskStat.Total/1024/1024/1024)
-	fmt.Printf("Free: %v GB\n", diskStat.Free/1024/1024/1024)
-	fmt.Printf("Used: %v GB\n", diskStat.Used/1024/1024/1024)
-	fmt.Printf("Used Percent: %0.3f%%\n", diskStat.UsedPercent)
-
-	fmt.Println("\n==================")
-	fmt.Printf("GPU Information\n")
-	fmt.Println("==================")
-	switch runtime.GOOS {
-	case "linux":
-		nvidiaInfo := getGPUInfoNvidia()
-		if strings.Contains(nvidiaInfo, "Error") {
-			fmt.Println(getGPUInfoLinux())
-		} else {
-			fmt.Println(nvidiaInfo)
-		}
-	case "windows":
-		fmt.Println(getGPUInfoWindows())
-	case "darwin": // macOS
-		fmt.Println(getGPUInfoMacOS())
-	default:
-		fmt.Println("GPU information not available for this OS.")
-	}
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/systeminfo", systemInfoHandler)
+	http.ListenAndServe(":8080", nil)
 }
